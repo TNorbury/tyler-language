ab6e7b800f27fdb87fd2f9e7333a8360f78ddbbd
Tyler Norbury <tylernorbury96@gmail.com>
2020-08-05 21:33:10 -0700

    add a package for cl args

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
-use std::env;
+use clap::{App, Arg};
 use std::fs;
-use std::process;
 
 mod language_processor;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    const TYLER_FILE_KEY: &str = "FILE";
 
-    if args.len() != 2 {
-        println!("You must provide a tyler file to execute!");
-        process::exit(1);
-    }
-    let file = &args[1];
+    // Set up the command line arguments
+    let cl_args = App::new("tyler")
+        .version("0.1.0")
+        .author("tnorbury")
+        .about("Interpreter for the tyler language")
+        .arg(
+            Arg::new(TYLER_FILE_KEY)
+                .about("The tyler file to run")
+                .value_name("tyler_file")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    // extract the tyler file from the command line arguments
+    let file = cl_args.value_of(TYLER_FILE_KEY).unwrap();
     println!("Executing file {}\n", file);
 
     // read in the file to execute, and create a vector of line
